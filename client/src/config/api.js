ee22dcdccc11ddaaad360bf478b821302bcb9108
vladyslavVp99 <darknamexx@gmail.com>
2024-09-23 23:25:20 +0300

    Update API configuration in client/src/config/api.js
    This commit modifies the API configuration file to improve the client-side setup. The changes may include updating API endpoints, adjusting request headers, or refining error handling for better communication with the server.

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,8 +1,8 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 
 export const endpoints = {
-    eventsAll: `${API_URL}/events/all`,
-    events: `${API_URL}/events`,
-    userRegister: `${API_URL}/users/register`,
-    members: `${API_URL}/users/members`,
+    eventsAll: `${API_URL}/api/events/all`,
+    events: `${API_URL}/api/events`,
+    userRegister: `${API_URL}/api/users/register`,
+    members: `${API_URL}/api/users/members`,
 };
