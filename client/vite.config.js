68fbb98e370f0341ee5debc725ac2f33e781785e
vladyslavVp99 <darknamexx@gmail.com>
2024-09-23 21:51:55 +0300

    Update .gitignore and client/vite.config.js
    - Modify .gitignore to exclude additional files or directories
    - Adjust configuration in client/vite.config.js for improved build process

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,11 @@ export default defineConfig({
   server: {
     port: 3001,
     proxy: {
-      '/api': 'http://localhost:3100',
+      '/api': {
+        target: 'https://event-registration-app-kvd7.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
     },
   },
 })
