db0593fe992177b56bd45e8b2112bd979344dd39
vladyslavVp99 <darknamexx@gmail.com>
2024-09-30 02:07:20 +0300

    Update! ~ Changed cluster to store the data in the database

--- server/assets/script/api.urls.js
+++ server/assets/script/api.urls.js
@@ -2,33 +2,23 @@ const fetchUrls = [
     {
         "url": "https://www.eventbrite.com/api/v3/destination/events/",
         "params": {
-            "event_ids": "918800165027,927753594977,920310703087,881980135327,63049080497,984065194487,956317339907,165655358637",
-            "expand": "event_sales_status,image,primary_venue,saves,ticket_availability,primary_organizer,public_collections",
-            "page_size": 50,
-            "include_parent_events": true
-        }
-    },
-    {
-        "url": "https://www.eventbrite.com/api/v3/destination/events/",
-        "params": {
-            "event_ids": "1014286025807,1014286005747,1001702778977,947244121667,1014285845267,970947679647,1015212376547",
-            "page_size": 7,
+            "event_ids": "926379204137,938992932137,910900817867,852593639527,1015622884387,928728771757,929315045317,938590608777,991085963797,743620036547,986167061227,950893025637,1015709964847,771452815137,944711917777,974386394937,974387167247,1002567003897,965481630547,993898997657",
+            "page_size": 20,
             "expand": "event_sales_status,image,primary_venue,saves,ticket_availability,primary_organizer,public_collections"
         }
     },
     {
         "url": "https://www.eventbrite.com/api/v3/destination/events/",
         "params": {
-            "event_ids": "930080544947,782231233677,934955064767,799052546677,929600579357,892122912647,762987144087,950160875757",
-            "expand": "event_sales_status,image,primary_venue,saves,ticket_availability,primary_organizer,public_collections",
-            "page_size": 50,
-            "include_parent_events": true
+            "event_ids": "927893954797,880943655187,858948065787,998658844487,1007791620867,875458739657,1028391274987,906737475197,1000929536187,927557829437,931980086527,991339782977,1023845909677,1012895005227,907238132677,999479308517,886381540047,881507772477,1006933273527,1010109393387",
+            "page_size": 20,
+            "expand": "event_sales_status,image,primary_venue,saves,ticket_availability,primary_organizer,public_collections"
         }
     },
     {
         "url": "https://www.eventbrite.com/api/v3/destination/events/",
         "params": {
-            "event_ids": "920293541757,926379204137,944711917777,925453726007,998177023347,938992932137,946484409347,933525759677,974393987647,927515091607,923152643407,981848955657,986167061227,1015622884387,771452815137,928728771757,865453864807,1000919686727,938590608777,974385682807",
+            "event_ids": "1008098017307,952703821777,794434604287,951746959777,945164692037,876368310207,907772801887,964319604897,1010892014227,794438074667,921463481077,1023151933977,998624401467,925583494147,978590820487,794442317357,1001811674687,1012539822867,920350963507,1002391669467",
             "page_size": 20,
             "expand": "event_sales_status,image,primary_venue,saves,ticket_availability,primary_organizer,public_collections"
         }
