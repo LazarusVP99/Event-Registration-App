fc18a46fd6ce83b5f676aa12ae89ccb333d2e1f7
vladyslavVp99 <darknamexx@gmail.com>
2024-09-30 03:11:43 +0300

    Update! ~ Changed cluster to store the data in the database

--- server/assets/script/cron.task.js
+++ server/assets/script/cron.task.js
@@ -6,7 +6,7 @@ import { fetchAndStoreEvents } from './fetch.events.js';
 dotenv.config();
 
 async function startEventFetcher () {
-    await mongoose.connect(`mongodb+srv://vladvip8acc041022:pSzFKk5vfOG7kt2J@data-0.um1kono.mongodb.net/?retryWrites=true&w=majority&appName=data-0`)
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_CREDENTIALS}&appName=data-0`)
         .then(() => console.log('Successfully connected to MongoDB'))
         .catch(err => console.error('Connection error', err));
 
